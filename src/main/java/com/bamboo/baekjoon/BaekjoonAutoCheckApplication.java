f54a840adcb9934a58c071b2f96fdaa91e93989d
BambooKim <rlaqjarn1008@gmail.com>
2022-07-17 16:25:14 +0900

    chore: Spring Boot Project Init.

---
+++ src/main/java/com/bamboo/baekjoon/BaekjoonAutoCheckApplication.java
@@ -0,0 +1,14 @@
+package com.bamboo.baekjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BaekjoonAutoCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekjoonAutoCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamboo/baekjoon/BaekjoonAutoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamboo.baekjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekjoonAutoCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
