7bd3d8d12771b7aecbfb2643c177e380d1606d1c
BambooKim <rlaqjarn1008@gmail.com>
2022-07-17 19:55:26 +0900

    feat: SwaggerConfig

---
+++ src/main/java/com/bamboo/baekjoon/global/swagger/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.bamboo.baekjoon.global.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String API_NAME = "BaekjoonAutoCheck";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "백준 자율스터디 자동 채점 API";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .contact(new Contact("BambooKim", "https://github.com/BambooKim", "rlaqjarn1008@gmail.com"))
+                .build();
+    }
+}
