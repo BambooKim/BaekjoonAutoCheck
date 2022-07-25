9a49068527a70eb2c5b2261018d6e5574057ceea
BambooKim <rlaqjarn1008@gmail.com>
2022-07-25 15:21:37 +0900

    fix: N+1 Problem using @EntityGraph

--- src/main/java/com/bamboo/baekjoon/domain/checks/repository/CheckRepository.java
+++ src/main/java/com/bamboo/baekjoon/domain/checks/repository/CheckRepository.java
@@ -1,7 +1,25 @@
 package com.bamboo.baekjoon.domain.checks.repository;
 
 import com.bamboo.baekjoon.domain.checks.Checks;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CheckRepository extends JpaRepository<Checks, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"user", "term"})
+    List<Checks> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"user", "term"})
+    Page<Checks> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"user", "term"})
+    Optional<Checks> findById(Long aLong);
 }
