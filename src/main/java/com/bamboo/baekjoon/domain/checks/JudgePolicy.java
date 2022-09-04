7fb1648511414e31ec68cf2e15fb2cd4c157733d
BambooKim <rlaqjarn1008@gmail.com>
2022-09-04 15:14:40 +0900

    feat: JudgePolicy 변경

--- src/main/java/com/bamboo/baekjoon/domain/checks/JudgePolicy.java
+++ src/main/java/com/bamboo/baekjoon/domain/checks/JudgePolicy.java
@@ -5,13 +5,20 @@ import com.bamboo.baekjoon.domain.user.User;
 public class JudgePolicy {
 
     public static boolean isSubmitPass(User user, int probTier) {
-        if (user.getEnterYear() == 2022) {
+        if (user.getEnterYear() == 2022) {      // 22학번
             return true;
-        } else {
-            if (probTier >= (user.getUserTier() - 5)) {
+        } else {                                // ~21학번
+            if (user.getUserTier() <= 5) {      // B5 ~ B1
                 return true;
-            } else {
-                return false;
+            } else {                            // S5 ~
+                if (probTier >= (user.getUserTier() - 5)) {
+                    if (probTier <= 5)
+                        return false;
+                    else
+                        return true;
+                } else {
+                    return false;
+                }
             }
         }
     }
